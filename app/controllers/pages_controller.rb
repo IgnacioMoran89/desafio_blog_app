2305eb48f0f4ae09096be97f525bda21b7686cc6
Ignacio Moran <moran.ua@gmail.com>
2021-05-07 14:13:45 -0400

    controllers changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only: :dashboard
 
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @post = Post.all.order(created_at: :desc)
   end
 
   def dashboard
