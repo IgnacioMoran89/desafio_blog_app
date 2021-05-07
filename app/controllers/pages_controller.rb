c0d4e9533a12f84aa6d94ea49a733b5a8641f91f
Ignacio Moran <moran.ua@gmail.com>
2021-05-07 14:58:33 -0400

    heroku update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only: :dashboard
 
   def index
-    @post = Post.all.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def dashboard
