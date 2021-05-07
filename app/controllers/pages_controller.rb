5f43acc9a2763f0d8a26c3ecf9d01907ecc6fb5a
Ignacio Moran <moran.ua@gmail.com>
2021-05-06 22:54:03 -0400

    Dashboard created

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+  http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only: :dashboard
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def dashboard
+    @post = Post.new
+  end
+  
+  def create
+  end 
+end
